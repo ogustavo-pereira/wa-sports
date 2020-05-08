c0724b16538f142d6949bbb3b27800fe990da6d7
oguhpereira <guhpereira@outlook.com.br>
2020-05-08 17:34:44 -0300

    [FEAT] Add React with Layout

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import LOGO from '../../images/wasports.png';
+
+export default function Header() {
+	return (
+		<div id="header">
+			<div className="logo">
+				<img src={LOGO} alt="waSports" />
+			</div>
+		</div>
+	);
+}
